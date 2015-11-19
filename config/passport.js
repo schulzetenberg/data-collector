6a02e1160969abd9c8c08809b9890a4ab92ef9ed
Jacob Schulzetenberg <Jacob.Schulzetenberg@gmail.com>
2015-11-18 20:31:52 -0700

    Add hackathon-starter

---
+++ config/passport.js
@@ -0,0 +1,69 @@
+var _ = require('lodash');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var secrets = require('./secrets');
+var User = require('../models/User');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+/**
+ * Sign in using Email and Password.
+ */
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  email = email.toLowerCase();
+  User.findOne({ email: email }, function(err, user) {
+    if (!user) return done(null, false, { message: 'Email ' + email + ' not found'});
+    user.comparePassword(password, function(err, isMatch) {
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Invalid email or password.' });
+      }
+    });
+  });
+}));
+
+/**
+ * OAuth Strategy Overview
+ *
+ * - User is already logged in.
+ *   - Check if there is an existing account with a provider id.
+ *     - If there is, return an error message. (Account merging not supported)
+ *     - Else link new OAuth account with currently logged-in user.
+ * - User is not logged in.
+ *   - Check if it's a returning user.
+ *     - If returning user, sign in and we are done.
+ *     - Else check if there is an existing account with user's email.
+ *       - If there is, return an error message.
+ *       - Else create a new account.
+ */
+
+/**
+ * Login Required middleware.
+ */
+exports.isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+};
+
+/**
+ * Authorization Required middleware.
+ */
+exports.isAuthorized = function(req, res, next) {
+  var provider = req.path.split('/').slice(-1)[0];
+
+  if (_.find(req.user.tokens, { kind: provider })) {
+    next();
+  } else {
+    res.redirect('/auth/' + provider);
+  }
+};
---
+++ config/secrets.js
@@ -0,0 +1,69 @@
+/**
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ *
+ * You should never commit this file to a public repository on GitHub!
+ * All public code on GitHub can be searched, that means anyone can see your
+ * uploaded secrets.js file.
+ *
+ * I did it for your convenience using "throw away" API keys and passwords so
+ * that all features could work out of the box.
+ *
+ * Use config vars (environment variables) below for production API keys
+ * and passwords. Each PaaS (e.g. Heroku, Nodejitsu, OpenShift, Azure) has a way
+ * for you to set it up from the dashboard.
+ *
+ * Another added benefit of this approach is that you can use two different
+ * sets of keys for local development and production mode without making any
+ * changes to the code.
+
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ */
+
+module.exports = {
+
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+
+  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+
+  mailgun: {
+    user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
+    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
+  },
+  
+  mandrill: {
+    user: process.env.MANDRILL_USER || 'hackathonstarterdemo',
+    password: process.env.MANDRILL_PASSWORD || 'E1K950_ydLR4mHw12a0ldA'
+  },
+
+  sendgrid: {
+    user: process.env.SENDGRID_USER || 'hslogin',
+    password: process.env.SENDGRID_PASSWORD || 'hspassword00'
+  },
+
+  nyt: {
+    key: process.env.NYT_KEY || '9548be6f3a64163d23e1539f067fcabd:5:68537648'
+  },
+
+  twilio: {
+    sid: process.env.TWILIO_SID || 'AC6f0edc4c47becc6d0a952536fc9a6025',
+    token: process.env.TWILIO_TOKEN || 'a67170ff7afa2df3f4c7d97cd240d0f3'
+  },
+
+  clockwork: {
+    apiKey: process.env.CLOCKWORK_KEY || '9ffb267f88df55762f74ba2f517a66dc8bedac5a'
+  },
+
+  stripe: {
+    secretKey: process.env.STRIPE_SKEY || 'sk_test_BQokikJOvBiI2HlWgH4olfQ2',
+    publishableKey: process.env.STRIPE_PKEY || 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'
+  }, 
+
+  lob: {
+    apiKey: process.env.LOB_KEY || 'test_814e892b199d65ef6dbb3e4ad24689559ca'
+  },
+
+  bitgo: {
+    accessToken: process.env.BITGO_ACCESS_TOKEN || '4fca3ed3c2839be45b03bbd330e5ab1f9b3989ddd949bf6b8765518bc6a0e709'
+  }
+
+};
