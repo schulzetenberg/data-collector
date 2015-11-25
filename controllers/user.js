d2d1c6fb65574921138037bff192aa055fccbba8
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2015-11-24 20:39:08 -0700

    Fix flash messages

--- controllers/user.js
+++ controllers/user.js
@@ -175,26 +175,6 @@ exports.postDeleteAccount = function(req, res, next) {
 };
 
 /**
- * GET /account/unlink/:provider
- * Unlink OAuth provider.
- */
-exports.getOauthUnlink = function(req, res, next) {
-  var provider = req.params.provider;
-  User.findById(req.user.id, function(err, user) {
-    if (err) return next(err);
-
-    user[provider] = undefined;
-    user.tokens = _.reject(user.tokens, function(token) { return token.kind === provider; });
-
-    user.save(function(err) {
-      if (err) return next(err);
-      req.flash('info', { msg: provider + ' account has been unlinked.' });
-      res.redirect('/account');
-    });
-  });
-};
-
-/**
  * GET /reset/:token
  * Reset Password page.
  */
