d2d67c812060f79ebd4dd70af784879136fc420c
Schulzetenberg, Jacob <jacob.schulzetenberg@gmail.com>
2017-05-16 13:54:29 -0600

    Set mongoose promise library

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,29 +1,32 @@
 var mongoose = require('mongoose');
 var secrets = require('../config/secrets');
 
+// q promise library
+mongoose.Promise = require('q').Promise;
+
 var url = secrets.db;
 mongoose.connect(url);
 var db = mongoose.connection;
 
 // When successfully connected
-db.on('connected', function () {  
+db.on('connected', function () {
   console.log('Mongoose connection open to ' + url);
-}); 
-
-// If the connection throws an error
-db.on('error',function (err) {  
-  console.log('Mongoose connection error: ' + err);
-}); 
-
-// When the connection is disconnected
-db.on('disconnected', function () {  
-  console.log('Mongoose disconnected'); 
 });
 
-// If the Node process ends, close the Mongoose connection 
-process.on('SIGINT', function() {  
-  db.close(function () { 
-    console.log('Mongoose connection disconnected through app termination'); 
-    process.exit(0); 
-  }); 
-}); 
+// If the connection throws an error
+db.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+// When the connection is disconnected
+db.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  db.close(function () {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
+});
