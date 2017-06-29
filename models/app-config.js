2b12c88b1f0fea6aa082e58119b316d9fd9f29fc
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2017-06-28 20:56:13 -0600

    Add defaults to config

--- models/app-config.js
+++ models/app-config.js
@@ -2,46 +2,46 @@ var mongoose = require('mongoose');
 
 var appConfigSchema = new mongoose.Schema({
   github: {
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/github' },
+    functionName: { type: String, default: 'save' },
     active: { type: Boolean, default: false },
-    user: { type: String, default: '' },
+    user: { type: String, default: 'schulzetenberg' },
     token: { type: String, default: '' },
-    schedule: { type: String, default: '' }
+    schedule: { type: String, default: '5 0 0 * * *' }
   },
   goodreads: {
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/goodreads' },
+    functionName: { type: String, default: 'save' },
     active: { type: Boolean, default: false },
-    numDays: { type: Number, default: 0 },
+    numDays: { type: Number, default: 185 },
     id: { type: Number, default: 0 },
     key: { type: String, default: '' },
-    schedule: { type: String, default: '' }
+    schedule: { type: String, default: '5 0 0 * * *' }
   },
   music: {
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/music' },
+    functionName: { type: String, default: 'save' },
     active: { type: Boolean, default: false },
     lastFmKey: { type: String, default: '' },
     spotifyId: { type: String, default: '' },
     spotifySecret: { type: String, default: '' },
-    schedule: { type: String, default: '' }
+    schedule: { type: String, default: '5 0 0 * * *' }
   },
   trakt: {
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/trakt' },
+    functionName: { type: String, default: 'save' },
     active: { type: Boolean, default: false },
     id: { type: String, default: '' },
     key: { type: String, default: '' },
-    user: { type: String, default: '' },
-    schedule: { type: String, default: '' }
+    user: { type: String, default: 'waterland15' },
+    schedule: { type: String, default: '5 0 0 * * *' }
   },
   tmdb: {
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/tmdb' },
+    functionName: { type: String, default: 'getConfig' },
     active: { type: Boolean, default: false },
     key: { type: String, default: '' },
-    schedule: { type: String, default: '' }
+    schedule: { type: String, default: '20 0 0 */3 * *' }
   },
   states: {
     visited: { type: [] },
@@ -55,10 +55,10 @@ var appConfigSchema = new mongoose.Schema({
         url: ''
       }]
     },
-    filePath: { type: String, default: '' },
-    functionName: { type: String, default: '' },
+    filePath: { type: String, default: 'nodejs/fuelly' },
+    functionName: { type: String, default: 'save' },
     active: { type: Boolean, default: false },
-    schedule: { type: String, default: '' }
+    schedule: { type: String, default: '5 0 0 * * *' }
   },
   arrayObj: {
     arr: { type: [], default:
