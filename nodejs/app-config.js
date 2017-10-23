b95b41d4b62d6ab37677f05f334330ad7b0ced51
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2017-10-23 11:36:19 -0600

    Code cleanup

--- nodejs/app-config.js
+++ nodejs/app-config.js
@@ -1,9 +1,9 @@
-var Q = require('q');
+const Q = require('q');
 
-var configModel = require('../models/app-config');
+const configModel = require('../models/app-config');
 
 exports.get = function(){
-  return configModel.findOne({}, '', {sort: {'_id' : -1}}).lean().exec();
+  return configModel.findOne({}, '', {sort: {'_id' : -1}}).lean();
 };
 
 exports.app = function(filter){
@@ -11,14 +11,18 @@ exports.app = function(filter){
 
   filter = filter ? filter : ''; // Set filter to '' if not specified
 
-  configModel.findOne({}, filter, {sort: {'_id' : -1}}).lean().exec(function(err, data) {
-    if(err) return defer.reject(err);
-    if(!data) return defer.reject("No config saved in db");
-    if(filter && !data[filter]) return defer.reject("No config found for '" + filter + "'");
-    if(filter) data = data[filter];
-
-    defer.resolve(data);
-  });
+  configModel.findOne({}, filter, {sort: {'_id' : -1}}).lean().then(function(data) {
+    if(!data) {
+      defer.reject('No config saved in db');
+    } else if(filter && !data[filter]) {
+      defer.reject('No config found for "' + filter + '"');
+    } else {
+      if(filter) data = data[filter];
+      defer.resolve(data);
+    }
+  }).catch(function(err) {
+    defer.reject(err);
+  })
 
   return defer.promise;
 };
--- nodejs/github.js
+++ nodejs/github.js
@@ -1,72 +1,77 @@
-var request = require('request');
-var Q = require('q');
+const request = require('request');
+const Q = require('q');
 
-var logger = require('./log');
-var githubModel = require('../models/github-model.js');
-var appConfig = require('./app-config');
+const logger = require('./log');
+const githubModel = require('../models/github-model.js');
+const appConfig = require('./app-config');
 
 exports.save = function() {
-  logger.info("Starting Github");
+  logger.info('Starting Github');
 
-  appConfig.get().then(function(config){
-    if(config && config.github && config.github.user && config.github.token){
-      var promises = [userData(config.github),contribData(config.github)];
+  appConfig.app('github').then(function(githubConfig) {
+    if(githubConfig && githubConfig.user && githubConfig.token) {
+      var promises = [
+        userData(githubConfig),
+        contribData(githubConfig)
+      ];
+
       Q.all(promises).then(function(data){
         var doc = new githubModel({
           repos: data[0].public_repos,
           contribSvg: data[1]
         });
 
-        doc.save(function(err) {
-          if (err) logger.error(err);
+        doc.save().catch(function(err) {
+          logger.error(err);
         });
+      }).then(function(){
+        logger.info('Done with Github save');
       }).catch(function(err){
-        logger.error("Caught github save error:", err);
+        logger.error('Caught github save error:', err);
       });
     } else {
-      logger.error("Github config missing");
+      logger.error('Github config missing');
     }
   }).catch(function(err){
-    logger.error("Github config error:", err);
+    logger.error('Github config error:', err);
+  });
+};
+
+function userData(config){
+  var defer = Q.defer();
+
+  var options = {
+    url: 'https://api.github.com/users/' + config.user + '?access_token=' + config.token,
+    headers: { 'User-Agent': 'GitHub User:' + config.user }
+  };
+
+  request(options, function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      defer.reject(error);
+    } else {
+      try {
+        body = JSON.parse(body);
+        defer.resolve(body);
+      } catch (e){
+        defer.reject('Unable to parse github response body', e);
+      }
+    }
   });
 
-  function userData(config){
-    var defer = Q.defer();
+  return defer.promise;
+}
 
-    var options = {
-      url: "https://api.github.com/users/" + config.user + "?access_token=" + config.token,
-      headers: { 'User-Agent': 'GitHub User:' + config.user }
-    };
+function contribData(config){
+  var defer = Q.defer();
+  var optionsContrib = { url: 'https://github.com/users/' + config.user + '/contributions' };
 
-    request(options, function (error, response, body) {
-      if (error || response.statusCode !== 200) {
-        defer.reject(error);
-      } else {
-        try {
-          body = JSON.parse(body);
-          defer.resolve(body);
-        } catch (e){
-          defer.reject("unable to parse github response body", e);
-        }
-      }
-    });
+  request(optionsContrib, function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      defer.reject(error);
+    } else {
+      defer.resolve(body);
+    }
+  });
 
-    return defer.promise;
-  }
-
-  function contribData(config){
-    var defer = Q.defer();
-    var optionsContrib = { url: "https://github.com/users/" + config.user + "/contributions" };
-
-    request(optionsContrib, function (error, response, body) {
-      if (error || response.statusCode !== 200) {
-        defer.reject(error);
-      } else {
-        defer.resolve(body);
-      }
-    });
-
-    return defer.promise;
-  }
-
-};
+  return defer.promise;
+}
