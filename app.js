d0f5b218b2e38bd0753048b2f1ab899bd3cbd8eb
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2018-07-26 21:55:59 -0600

    Disable CORS

--- app.js
+++ app.js
@@ -77,6 +77,13 @@ app.use(expressValidator());
 app.use(methodOverride());
 app.use(cookieParser());
 
+// Disable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 function requireHTTPS(req, res, next) {
   if (!req.secure) {
     return res.redirect('https://' + req.get('host') + req.url);
