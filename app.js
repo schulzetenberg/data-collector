50296b264a35225677e838613f4f66bc51715cff
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2018-07-27 09:27:46 -0600

    Update CORS

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(cookieParser());
 // Disable CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-xsrf-token");
     next();
 });
 
