b1f5ea0f50997e775b678477f4a0c5e307915c1f
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2019-11-03 21:01:09 -0600

    Update deployment script

--- start-data-collector.sh
+++ start-data-collector.sh
@@ -1,7 +1,9 @@
-export SESSION_SECRET=123
+export SESSION_SECRET=SECRETHERE!!!!!!!
 export IP_ADDRESS=0.0.0.0
-export PORT=8998
+export PORT=3000
+export DB=mongodb://root:PASSWORDHERE!!!!!!!@localhost:27017/data-collector?authSource=admin
 
 npm i
 
+forever stopall
 forever start ./nodejs/server.js
