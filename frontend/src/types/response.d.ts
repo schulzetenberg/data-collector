012cce9cff1021e4be3defe4097efffa956d6ff8
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2021-03-23 16:38:58 -0500

    Update start script

--- frontend/src/types/response.d.ts
+++ frontend/src/types/response.d.ts
@@ -8,11 +8,4 @@ interface ReponseError {
   errors: string[];
 }
 
-interface ResponseValidationError {
-  location: string;
-  msg: string;
-  param: string;
-  value: string;
-}
-
 interface ServerResponse extends ResponseSuccess, ReponseError {}
--- start-data-collector.sh
+++ start-data-collector.sh
@@ -1,7 +1,6 @@
 export SESSION_SECRET=SECRETHERE!!!!!!!
 export IP_ADDRESS=0.0.0.0
-export PORT=3000
-export DB=mongodb://root:PASSWORDHERE!!!!!!!@localhost:27017/data-collector?authSource=admin
+export MongoUrl=mongodb://root:PASSWORDHERE!!!!!!!@localhost:27017/
 export CLOUDINARY_CLOUD_NAME=WRONGCLOUDNAME!!!
 export CLOUDINARY_API_KEY=WRONGAPIKEY!!!
 export CLOUDINARY_API_SECRET=WRONGAPISECRET!!!
